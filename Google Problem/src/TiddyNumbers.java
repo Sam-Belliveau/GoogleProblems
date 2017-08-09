84e719900a21c0f09a16c5551510c8973ce8ed3a
Sampajama <sam.belliveau@gmail.com>
2017-08-09 18:27:43 -0400

    Update TiddyNumbers.java

--- Google Problem/src/TiddyNumbers.java
+++ Google Problem/src/TiddyNumbers.java
@@ -1,27 +1,20 @@
 import java.io.File;
 import java.util.Scanner;
 public class TiddyNumbers {
-	
-	public static void main(String[] args) throws Exception {
-		
+	public static void main(String[] args) throws Exception {	
 		Scanner input = new Scanner(new File("src/B-large-practice.in"));
-		
-		String str, n, T;
+		String str,  T;
 		T = input.nextLine();
 		char digits[] = {'0','1','2','3','4','5','6','7','8','9'};
 		int temp[] = new int[2];
-		
 		for(int tt = 1; tt <= Integer.parseInt(T); tt++){
-			n = input.nextLine();
-			str = n;
+			str = input.nextLine();
 			char convert [] = str.toCharArray();
 			for(int i = 0; i < convert.length; i++){
 				boolean carry = true;
 				for(int ii = 0; ii < convert.length-1; ii++){
-					
 					temp[0] = Character.getNumericValue(convert[ii]);
 					temp[1] = Character.getNumericValue(convert[ii+1]);
-					
 					if (temp[1]<temp[0]){ 
 						convert[ii+1] = '9'; 
 						if (carry){
@@ -29,7 +22,6 @@ public class TiddyNumbers {
 							carry = false;
 						}
 					}
-
 				}
 			}
 			System.out.print("Case #" + tt + ": ");
@@ -37,7 +29,8 @@ public class TiddyNumbers {
 				for(int i = 1; i < convert.length; i++){
 					System.out.print(convert[i]);
 				}
-			}else{
+			}
+			else{
 				for(int i = 0; i < convert.length; i++){
 					System.out.print(convert[i]);
 				}
@@ -46,5 +39,4 @@ public class TiddyNumbers {
 		}
 		input.close();
 	}
-
 }
