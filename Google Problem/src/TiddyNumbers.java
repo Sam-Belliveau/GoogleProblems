fa9afef34d32be1307f06cf35d4010443840ff92
Sampajama <sam.belliveau@gmail.com>
2017-08-09 18:29:05 -0400

    Update TiddyNumbers.java

--- Google Problem/src/TiddyNumbers.java
+++ Google Problem/src/TiddyNumbers.java
@@ -1,23 +1,30 @@
 import java.io.File;
 import java.util.Scanner;
-public class TiddyNumbers {
-	public static void main(String[] args) throws Exception {	
+public class TiddyNumbers
+{
+	public static void main(String[] args) throws Exception 
+	{	
 		Scanner input = new Scanner(new File("src/B-large-practice.in"));
 		String str,  T;
 		T = input.nextLine();
 		char digits[] = {'0','1','2','3','4','5','6','7','8','9'};
 		int temp[] = new int[2];
-		for(int tt = 1; tt <= Integer.parseInt(T); tt++){
+		for(int tt = 1; tt <= Integer.parseInt(T); tt++)
+		{
 			str = input.nextLine();
 			char convert [] = str.toCharArray();
-			for(int i = 0; i < convert.length; i++){
+			for(int i = 0; i < convert.length; i++)
+			{
 				boolean carry = true;
-				for(int ii = 0; ii < convert.length-1; ii++){
+				for(int ii = 0; ii < convert.length-1; ii++)
+				{
 					temp[0] = Character.getNumericValue(convert[ii]);
 					temp[1] = Character.getNumericValue(convert[ii+1]);
-					if (temp[1]<temp[0]){ 
+					if (temp[1]<temp[0])
+					{ 
 						convert[ii+1] = '9'; 
-						if (carry){
+						if (carry)
+						{
 							convert[ii] = digits[temp[0]-1];
 							carry = false;
 						}
@@ -25,13 +32,17 @@ public class TiddyNumbers {
 				}
 			}
 			System.out.print("Case #" + tt + ": ");
-			if (convert[0] == '0'){
-				for(int i = 1; i < convert.length; i++){
+			if (convert[0] == '0')
+			{
+				for(int i = 1; i < convert.length; i++)
+				{
 					System.out.print(convert[i]);
 				}
 			}
-			else{
-				for(int i = 0; i < convert.length; i++){
+			else
+			{
+				for(int i = 0; i < convert.length; i++)
+				{
 					System.out.print(convert[i]);
 				}
 			}
